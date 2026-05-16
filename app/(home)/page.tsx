7c49d19a286ad8bd23febce330fa63106b1aa9ba
Ryan-Zayne <phoenixastro97@gmail.com>
2026-05-16 02:20:35 +0100

    feat: section two

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,23 @@
 import { ForWithWrapper } from "@zayne-labs/ui-react/common/for";
 import Image from "next/image";
+import type React from "react";
 import { heroImg } from "@/assets/images/landing";
 import { IconBox } from "@/components/common/IconBox";
+import {
+	communityOutReachIcon,
+	educationIcon,
+	humanDevelopmentIcon,
+	mentorshipIcon,
+} from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import { cnJoin } from "@/lib/utils/cn";
 import { Main } from "./-components/Main";
 
 function HomePage() {
 	return (
-		<Main>
+		<Main className="gap-12 lg:gap-[80px]">
 			<HeroSection />
+			<WhatWeDoSection />
 		</Main>
 	);
 }
@@ -71,12 +80,106 @@ function HeroSection() {
 							className="flex h-[92px] w-[102px] flex-col justify-center gap-1 rounded-[16px]
 								bg-cedar-black px-4 text-cedar-yellow lg:w-[124px] lg:rounded-[12px] lg:px-7"
 						>
-							<h4 className="text-[32px]/[100%]">{stat.count}+</h4>
+							<h4 className="text-[32px]/none">{stat.count}+</h4>
 							<p className="text-[10px]/4 lg:text-[12px]">{stat.label}</p>
 						</li>
 					)}
 				/>
 			</article>
+		</section>
+	);
+}
+
+const offers: Array<{ description: string; icon: string; title: string }> = [
+	{
+		description: "Providing structured academic guidance and learning opportunities.",
+		icon: educationIcon,
+		title: "Education Support",
+	},
+	{
+		description: "Connecting young minds with mentors for guidance and growth",
+		icon: mentorshipIcon,
+		title: "Mentorship",
+	},
+	{
+		description: "Reaching deserved communities with impactful initiatives",
+		icon: communityOutReachIcon,
+		title: "Community Outreach",
+	},
+	{
+		description: "Equipping individuals with practical and vocational skills",
+		icon: humanDevelopmentIcon,
+		title: "Human Development",
+	},
+];
+
+function WhatWeDoSection() {
+	return (
+		<section
+			className="rounded-[24px] bg-cedar-black px-5 pt-7.5 pb-9.5 lg:mt-10 lg:rounded-[40px]
+				lg:px-[80px] lg:pt-11 lg:pb-[96px]"
+		>
+			<h2 className="text-center leading-none text-cedar-yellow lg:text-[24px]">What we do</h2>
+			<h3 className="mt-2 text-center text-[20px]/none text-cedar-white lg:mt-3 lg:text-[40px]">
+				Dismantling barriers, building futures.
+			</h3>
+			<ForWithWrapper
+				className="mt-9.5 grid grid-cols-2 grid-rows-[148px] items-stretch justify-center gap-2
+					lg:mt-[52px] lg:grid-cols-[repeat(4,minmax(min-content,245px))]
+					lg:grid-rows-[minmax(252px,1fr)] lg:gap-4.5"
+				each={offers}
+				renderItem={(offer, index) => {
+					const offerCount = index + 1;
+
+					const totalColumns = 2;
+
+					const rowNumber = Math.ceil(offerCount / totalColumns);
+
+					const columnPlacement = offerCount % totalColumns;
+
+					const colNumber = columnPlacement === 0 ? totalColumns : columnPlacement;
+
+					const isEvenRow = rowNumber % 2 === 0;
+
+					const swappedColNumber = isEvenRow ? totalColumns + 1 - colNumber : colNumber;
+
+					return (
+						<li
+							data-order={offerCount}
+							key={offer.title}
+							className={cnJoin(
+								`flex w-full shrink-0 flex-col justify-between gap-3 rounded-[16px]
+								bg-[hsl(240,5%,5%)] p-2 lg:rounded-[20px] lg:pt-4 lg:pr-3.5 lg:pb-6 lg:pl-4`,
+								isEvenRow && "max-lg:[grid-area:var(--grid-area)]"
+							)}
+							style={
+								isEvenRow ?
+									({ "--grid-area": `${rowNumber}/${swappedColNumber}` } as React.CSSProperties)
+								:	undefined
+							}
+						>
+							<span
+								className={cnJoin(
+									"w-fit rounded-[12px] p-3.5 lg:rounded-[20px] lg:p-6",
+									offerCount % 2 === 0 ? "bg-cedar-yellow" : "bg-cedar-red"
+								)}
+							>
+								<Image
+									width={20}
+									height={20}
+									src={offer.icon}
+									alt="icon"
+									className="size-5 lg:size-8"
+								/>
+							</span>
+							<h4 className="leading-none text-cedar-white lg:text-[24px]">{offer.title}</h4>
+							<p className="text-[10px]/[1.4] text-cedar-white/80 lg:text-[14px]">
+								{offer.description}
+							</p>
+						</li>
+					);
+				}}
+			/>
 		</section>
 	);
 }
--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,7 @@
 export { default as hamburgerIcon } from "./hamburger-icon.svg?override";
 export { default as xIcon } from "./x-icon.svg?override";
 export { default as spinnerIcon } from "./spinner-icon.svg?override";
+export { default as communityOutReachIcon } from "./community-outreach-icon.svg?override";
+export { default as educationIcon } from "./education-icon.svg?override";
+export { default as mentorshipIcon } from "./mentorship-icon.svg?override";
+export { default as humanDevelopmentIcon } from "./human-development-icon.svg?override";
