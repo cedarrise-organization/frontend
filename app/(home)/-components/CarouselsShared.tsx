ba82d289cfd6c9befd7b4bd761be08becbba39ff
Ryan-Zayne <phoenixastro97@gmail.com>
2026-06-03 21:49:53 +0100

    .

--- app/(home)/-components/CarouselsShared.tsx
+++ app/(home)/-components/CarouselsShared.tsx
@@ -101,10 +101,9 @@ export function MomentsCarouselShared(props: MomentsCarouselSharedProps) {
 						<Carousel.Item
 							key={galleryRowItem.public_id}
 							className={cnJoin(
-								"h-[240px] w-(--image-width) cursor-grab active:cursor-grabbing lg:h-[300px]",
+								"h-[240px] w-[50%] cursor-grab active:cursor-grabbing lg:h-[300px] lg:w-[30%]",
 								galleryRowItemIndex === galleryRow.length - 1 && "pr-5"
 							)}
-							style={{ "--image-width": `30%` } as React.CSSProperties}
 						>
 							<Image
 								id={galleryRowItem.public_id}
