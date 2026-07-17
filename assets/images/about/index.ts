163238eb7692ee912cf1394d2832c55723bc8cc8
Ryan-Zayne <phoenixastro97@gmail.com>
2026-07-17 16:38:49 +0100

    chore(house keeping): remove useless images

--- assets/images/about/index.ts
+++ assets/images/about/index.ts
@@ -1,6 +1,6 @@
 export { default as aboutCoreValueDesktopImg } from "./core-value-section-img-desktop.png";
 export { default as aboutCoreValueMobileImg } from "./core-value-section-img-mobile.png";
-export { default as aboutHeroImg } from "./hero-img-alt.jpg";
+export { default as aboutHeroImg } from "./hero-img.jpg";
 export { default as teamMemberOneImg } from "./team-member-1.jpg";
 export { default as teamMemberTwoImg } from "./team-member-2.svg?override";
 export { default as teamMemberThreeImg } from "./team-member-3.svg?override";
